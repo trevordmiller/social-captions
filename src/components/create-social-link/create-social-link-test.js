7b47357e8dcd9c07a6cb0781e83b9ceec381b9d6
Trevor D. Miller <trevordmiller@icloud.com>
2015-07-26 17:47:00 -0600

    Moving full examples to step 4 and replacing with generic examples for this step in the series.

--- src/components/create-social-link/create-social-link-test.js
+++ src/components/create-social-link/create-social-link-test.js
@@ -1,32 +1,17 @@
+// import assert from 'assert';
 import {assert} from 'chai';
-import createSocialLink from './create-social-link';
 
-describe('createSocialLink', () => {
+describe('moduleName', () => {
 
-	it('should return the correct twitter link pattern', () => {
-		let socialData = [
-			'twitter',
-			'www.youtube.com',
-			'Broadcast Yourself',
-			'videos'
-		];
+	describe('#methodName()', () => {
 
-		assert.equal(createSocialLink(...socialData), `https://twitter.com/share?url=www.youtube.com&text=Broadcast Yourself&hashtags=videos`);
+		it('should work', () => {
+			assert.equal(1 + 2, 2, 'the math is broken');
+		});
+
+		it('should work with deepEqual', () => {
+			let exampleString = 'Hello world';
+			assert.deepEqual(exampleString.split(' '), ['Hello', 'world']);
+		});
 	});
-
-	it('should return the correct linkedIn link pattern', () => {
-		let socialData = [
-			'linkedIn',
-			'www.youtube.com',
-			'Broadcast Yourself',
-			'videos'
-		];
-
-		assert.equal(createSocialLink(...socialData), `http://www.linkedin.com/shareArticle?url=www.youtube.com&title=Broadcast Yourself%20%23videos`);
-	});
-
-	// it('should work with deepEqual', () => {
-	// 	let exampleString = 'Hello world';
-	// 	assert.deepEqual(exampleString.split(' '), ['Hello', 'world']);
-	// });
 });
