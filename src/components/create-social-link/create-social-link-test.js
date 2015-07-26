63433dd9af250f4d759cf8ea825f9f7222db8f5b
Trevor D. Miller <trevordmiller@icloud.com>
2015-07-26 17:42:11 -0600

    Breaking out actual unit tests to step 4.

--- src/components/create-social-link/create-social-link-test.js
+++ src/components/create-social-link/create-social-link-test.js
@@ -1,27 +1,9 @@
-import assert from 'assert';
-import createSocialLink from './create-social-link';
+describe('moduleName', () => {
 
-describe('createSocialLink', () => {
+	describe('#methodName()', () => {
 
-	it('should return the correct twitter link pattern', () => {
-		let socialData = [
-			'twitter',
-			'www.youtube.com',
-			'Broadcast Yourself',
-			'videos'
-		];
-
-		assert.equal(createSocialLink(...socialData), `https://twitter.com/share?url=www.youtube.com&text=Broadcast Yourself&hashtags=videos`);
-	});
-
-	it('should return the correct linkedIn link pattern', () => {
-		let socialData = [
-			'linkedIn',
-			'www.youtube.com',
-			'Broadcast Yourself',
-			'videos'
-		];
-
-		assert.equal(createSocialLink(...socialData), `http://www.linkedin.com/shareArticle?url=www.youtube.com&title=Broadcast Yourself%20%23videos`);
+		it('should work', () => {
+			// assertions
+		});
 	});
 });
